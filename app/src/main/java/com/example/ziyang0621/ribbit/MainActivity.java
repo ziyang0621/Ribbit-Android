c2b916e86ae3c328d280c37acedf204eb19e7134
ziyang <ziyang0621@gmail.com>
2014-11-16 15:52:27 -0800

    project update

--- app/src/main/java/com/example/ziyang0621/ribbit/MainActivity.java
+++ app/src/main/java/com/example/ziyang0621/ribbit/MainActivity.java
@@ -212,6 +212,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
---
+++ app/src/main/java/com/example/ziyang0621/ribbit/ViewImageActivity.java
@@ -0,0 +1,40 @@
+package com.example.ziyang0621.ribbit;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class ViewImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+
+        ImageView imageView = (ImageView)findViewById((R.id.imageView));
+        Uri imageUri = getIntent().getData();
+
+        Picasso.with(this).load(imageUri.toString()).into(imageView);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
