6b03ca5cb189cdcaa786eaca6b382f8eb0851347
ziyang <ziyang0621@gmail.com>
2014-11-19 21:05:01 -0800

    project update

--- app/src/main/java/com/example/ziyang0621/ribbit/MainActivity.java
+++ app/src/main/java/com/example/ziyang0621/ribbit/MainActivity.java
@@ -207,7 +207,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             // this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setIcon(mSectionsPagerAdapter.getIcon(i))
                             .setTabListener(this));
         }
     }
